76cd916918b20a5a75057a3b03e7aaeb5792b64c
Lucas Vieira <lucasvieira@protonmail.com>
2019-04-27 16:27:47 -0300

    Make a small modification to test script

--- t/run-tests.lisp
+++ t/run-tests.lisp
@@ -1,11 +1,4 @@
-(require :asdf)
-
-(defmethod asdf:perform :around ((o asdf:load-op)
-                                 (c asdf:cl-source-file))
-   (handler-case (call-next-method o c)
-      (sb-ext:invalid-fasl ()
-         (asdf:perform (make-instance 'asdf:compile-op) c)
-         (call-next-method))))
-
 (ql:quickload :cl-jackc/test)
+(in-package :cl-jackc/test)
 (rove:run :cl-jackc/test)
+
